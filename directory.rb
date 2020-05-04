36085a8fd4af7d9031b8e5e9634c42f7ca09c2a9
Tim <timcpb@hotmail.com>
2020-05-04 23:03:58 +0000

    Used methods to make directory.rb more declarative

--- directory.rb
+++ directory.rb
@@ -12,11 +12,19 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-#then printing them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
 end
-#finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+      puts name
+  end
+end
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+#so here we use the methods to print everything out
+print_header
+print(students)
+print_footer(students)
