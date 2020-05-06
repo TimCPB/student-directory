e867266a6aad847c0fb8af8b8069667631aeaaa0
Tim <timcpb@hotmail.com>
2020-05-06 10:28:34 +0000

    updated the draft version of directory.rb

---
+++ copy_of_directory.rb
@@ -0,0 +1,41 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #creating an ampty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  # while the name input isn't empty, do this code
+  while !name.empty? do
+    #make a hash that includes the name input and push it to the students array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get the next name
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+end
+def print(students)
+  no_of_students = 0
+  while no_of_students < students.length do
+    students.each_with_index do |student, index|
+      puts "#{(index + 1)}.  #{student[:name]} (#{student[:cohort]} cohort)"
+      no_of_students += 1
+    end
+  end
+end
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+#so here we use the methods to print everything out
+students = input_students
+#remember, the input_students method will return an array of hashes,
+print_header
+print(students)
+print_footer(students)
