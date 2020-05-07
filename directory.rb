f4101f5944c5033d61ea5d134e9a0eaa2de2d03e
Tim <timcpb@hotmail.com>
2020-05-07 22:38:20 +0000

    Sorted student list by cohort

--- directory.rb
+++ directory.rb
@@ -1,11 +1,20 @@
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just type end"
   students = []
   name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+  while name != "end"
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    puts "Which cohort are they in?"
+    cohort = gets.chomp
+    if cohort.empty? || !months.include?(cohort)
+      cohort = "January"
+    end
+    if name.empty?
+      name = "Mystery Student"
+    end
+    students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{students.count} students, please enter another name or type end to finish"
     name = gets.chomp
   end
   students
@@ -16,9 +25,18 @@ def print_header
     puts "-------------".center(50," ")
 end
 def print(students)
+  sorted_by_cohort = {}
   students.each do |student|
-      output = "#{student[:name]} (#{student[:cohort]} cohort)"
-      puts output.center(50," ")
+      cohort = student[:cohort]
+      name = student[:name]
+      if sorted_by_cohort[cohort] == nil
+        sorted_by_cohort[cohort] = [name]
+      else
+        sorted_by_cohort[cohort].push(name)
+      end
+  end
+  sorted_by_cohort.each do |key, value_array|
+    puts "#{key} cohort: #{value_array.join(", ")}".center(60," ")
   end
 end
 def print_footer(names)
