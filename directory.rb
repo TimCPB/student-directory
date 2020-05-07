c32ef265bf606d50f53baed4251d420e69fff800
Tim <timcpb@hotmail.com>
2020-05-07 12:57:49 +0000

    Centred and aligned the output

--- directory.rb
+++ directory.rb
@@ -1,47 +1,31 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #creating an ampty array
   students = []
-  #get the first name
   name = gets.chomp
-  # while the name input isn't empty, do this code
   while !name.empty? do
-    p "How old is this student?"
-    age = gets.chomp
-    p "How tall is this student?"
-    height = gets.chomp
-    p "please enter their hobbies"
-    hobbies = gets.chomp
-    #make a hash that includes the name input and push it to the students array
-    students << {name: name, cohort: :november, age: age, height: height, hobbies: hobbies}
-    puts "Now we have #{students.count} students, please enter another name or hit return to finish"
-    #get the next name
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
     name = gets.chomp
   end
-  #return the array of students
   students
 end
 
 def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+    puts "The students of Villains Academy".center(50," ")
+    puts "-------------".center(50," ")
 end
 def print(students)
-  no_of_students = 0
-  while no_of_students < students.length do
-    students.each_with_index do |student, index|
-      puts "#{(index + 1)}.  #{student[:name]} (#{student[:cohort]} cohort): height: #{student[:height]}, age: #{student[:age]}, hobbies: #{student[:hobbies]}"
-      no_of_students += 1
-    end
+  students.each do |student|
+      output = "#{student[:name]} (#{student[:cohort]} cohort)"
+      puts output.center(50," ")
   end
 end
 def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+    puts ""
+    puts "Overall, we have #{names.count} great students".center(50," ")
 end
-#so here we use the methods to print everything out
 students = input_students
-#remember, the input_students method will return an array of hashes,
 print_header
 print(students)
 print_footer(students)
