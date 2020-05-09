a8eebfd2994435009d595ca805a6476c15380f71
Tim <timcpb@hotmail.com>
2020-05-09 13:48:38 +0000

    Simplified the functions of my code back to match the example code to make the upcoming exercises simpler

--- directory.rb
+++ directory.rb
@@ -1,66 +1,40 @@
-@students = []
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just type end"
-  
-  name = gets.gsub(/\n/,"")
-  while name != "end"
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    puts "Which cohort are they in?"
-    cohort = gets.gsub(/\n/,"")
-    if cohort.empty? || !months.include?(cohort)
-      cohort = "January"
-    end
-    if name.empty?
-      name = "Mystery Student"
-    end
-    @students << {name: name, cohort: cohort.to_sym}
-    if @students.count == 1
-      puts "Now we have 1 student, enter another student or type end to finish"
-    else
-      puts "Now we have #{@students.count} students, enter another student or type end to finish"
-    end
-    name = gets.gsub(/\n/,"")
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = gets.chomp
   end
-  @students
 end
 
-def print_header
-    puts "The students of Villains Academy".center(50," ")
-    puts "-------------".center(50," ")
-end
-def print_students_list
-  if !@students.empty?
-    sorted_by_cohort = {}
-    @students.each do |student|
-      cohort = student[:cohort]
-        name = student[:name]
-        if sorted_by_cohort[cohort] == nil
-          sorted_by_cohort[cohort] = [name]
-        else
-          sorted_by_cohort[cohort].push(name)
-        end
-  end
-  sorted_by_cohort.each do |key, value_array|
-    puts "#{key} cohort: #{value_array.join(", ")}".center(60," ")
-  end
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
-def print_footer
-    puts ""
-    puts "Overall, we have #{@students.count} great students".center(50," ")
-end
+
 def print_menu
-  puts "1. Input the sudents"
+  puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the lsit to students.csv"
-  puts "9. Exit" #because we'll be adding more lines later
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
 end
+
 def show_students
   print_header
-  print_students_list
+  print_student_list
   print_footer
 end
+
 def save_students
   #open the file for writing, save it to the vaiable file, "w" for write mode
   file = File.open("students.csv", "w")
@@ -72,31 +46,36 @@ def save_students
   end
   file.close
 end
+
 def process(selection)
   case selection
-   when "1"
-    @students = input_students
-   when "2"
+  when "1"
+    input_students
+  when "2"
     show_students
-   when "3"
+  when "3"
     save_students
-   when "9"
-    exit
-   else
-    puts "I don't know what you mean, please try again"
-  end
-end
-  
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
   end
 end
 
+def print_header
+  puts "The students of Villains Academy".center(60," ")
+  puts "-------------".center(60," ")
+end
 
-    
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(60," ")
+  end
+end
+
+def print_footer
+  puts ""
+  puts "Overall, we have #{@students.count} great students".center(60," ")
+end
 
 interactive_menu
-
-save_students
